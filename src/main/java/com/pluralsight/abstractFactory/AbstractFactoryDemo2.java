a674b5407515df1e8c5bde17fe575b82ed18d1ff
Singh <suraj.singh@here.com>
2020-04-21 21:31:13 +0530

    Small changes in client program of AbstractFactory

--- src/main/java/com/pluralsight/abstractFactory/AbstractFactoryDemo2.java
+++ src/main/java/com/pluralsight/abstractFactory/AbstractFactoryDemo2.java
@@ -1,17 +1,22 @@
 package main.java.com.pluralsight.abstractFactory;
 
+//Client side
 public class AbstractFactoryDemo2 {
 
     public static void main(String[] args) {
 
         CreditCardFactory abstractFactory = CreditCardFactory.getCreditCardFactory(775);
         CreditCard creditCard = abstractFactory.getCreditCard(CardType.PLATINUM);
+        Validator validator = abstractFactory.getValidator(CardType.PLATINUM);
 
         System.out.println(creditCard.getClass());
+        System.out.println(validator.getClass());
 
         abstractFactory = CreditCardFactory.getCreditCardFactory(600);
         CreditCard creditCard2 = abstractFactory.getCreditCard(CardType.GOLD);
+        Validator validator2 = abstractFactory.getValidator(CardType.GOLD);
 
         System.out.println(creditCard2.getClass());
+        System.out.println(validator2.getClass());
     }
 }
